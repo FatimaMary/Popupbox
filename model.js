655218b76e7a95c211ae3146983e6b1a273b223e
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-06-29 10:28:15 +0530

    initial commit

---
+++ model.js
@@ -0,0 +1,8 @@
+document.getElementById("open").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block";
+})
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+})
+
